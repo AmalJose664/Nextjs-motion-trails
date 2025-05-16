3d8f03dd507357a183222d26689c469c15111e89
AmalJose <amal446446@gmai.com>
2025-05-16 21:08:50 +0530

    progress upto useScroll

--- src/app/trials/motion-value/page.tsx
+++ src/app/trials/motion-value/page.tsx
@@ -1,22 +1,38 @@
 'use client'
-import {motion, useMotionValue, useMotionValueEvent } from 'motion/react'
+import {motion,  useMotionValue,  useSpring, useTransform } from 'motion/react'
+import { ChangeEvent } from 'react'
 const MotionValue = () => {
-	const xValue = useMotionValue(60) 
-	useMotionValueEvent(xValue,'animationStart', ()=>{
-		console.log('animation started on x');
-		
-	} )
-	useMotionValueEvent(xValue, 'change', (latest) => {
-		console.log('change', latest);
+	const xValue = useSpring(1) // or useMotionValue
+	const x = useMotionValue(0)
+	const y = useMotionValue(0)
 
-	})
+	const bgColor = useTransform(x, [-100, 100], ['#ff000', '#ff00ff'])
+
+
+	const onChangeHandler = (e:ChangeEvent<HTMLInputElement>)=>{
+		xValue.set(parseFloat(e.target.value))
+	}
   return (
 	<div>
 		MotionValue
-		  <motion.div className='box' style={{ x: xValue }} drag dragConstraints={{top: -100, bottom: 200, left: -200, right:200}}>
+		  <motion.div className='box' style={{ scale: xValue }} drag dragConstraints={{top: -100, bottom: 200, left: -200, right:200}}>
 
 		</motion.div>
+		<div>
+			<div className='mt-[6rem]'>
+				<input type="range" min={.5} max={5} step={.01} defaultValue={1} onChange={onChangeHandler}/>
+			</div>
+		</div>
+		use Transform example
 
+		<div>
+			<motion.div 
+				  drag dragConstraints={{ left: -200, right: 200, top: -200, bottom: 200 }}
+				  style={ { x, y, backgroundColor:bgColor}}
+			className='w-32 h-32 flex items-center justify-center rounded-lg shadow-lg cursor-pointer'>
+				<span className='text-white'>Drag me</span>
+			</motion.div>
+		</div>
 	</div>
   )
 }
--- src/app/trials/page.tsx
+++ src/app/trials/page.tsx
@@ -10,6 +10,8 @@ const TrialsPage = () => {
 		<Link className='underline' href={'/trials/gesture'}> Gestures</Link> <br />
 		<Link className='underline' href={'/trials/stagger'}> Stagger</Link> <br />
 		<Link className='underline' href={'/trials/motion-value'}> Motion value </Link> <br />
+		<Link className='underline' href={'/trials/scroll'}> Scroll aniamtion </Link> <br />
+		<Link className='underline' href={'/trials/useScroll'}> Scroll aniamtion Use scroll </Link> <br />
 	</div>
   )
 }
